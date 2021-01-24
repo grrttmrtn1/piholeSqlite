445e6a7ba69aecacb622984c10f800c863f31411
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-01-24 15:49:25 -0600

    Create piholeSqlite.py

---
+++ piholeSqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+from sqlite3 import Error
+
+class piholeSqlite:
+
+    def __init__(self, db_file, query):
+        self.db_file = db_file
+        self.query = query
+
+    def create_connection (self):
+        try:
+            conn = sqlite3.connect (self.db_file)
+            return conn
+        except Error as e:
+            print (e)
+        return None
+
+    def query (self, conn):
+        cur = conn.cursor ()
+        conn.text_factory = str
+        cur.execute (self.query)
+        rows = cur.fetchall ()
+        return rows
